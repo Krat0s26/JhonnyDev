1c9c81f1c8f82152b57e6b770d88b1ee2d682663
kratos <jp.sulv.7241843@gmail.com>
2026-06-19 13:39:02 -0400

    Initial portfolio website

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/profile")
-async def profile():
-    return {
-        "name": "Jhonny",
-        "job": "Data Center Technician",
-        "location": "Columbus"
-    }
-
-@app.get("/skills")
-async def skills():
-    return {
-        "skills": [
-            "Fiber Optics",
-            "Linux",
-            "Networking",
-            "Python"
-        ]
-    }
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
